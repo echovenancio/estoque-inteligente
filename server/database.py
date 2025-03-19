a0c9af640708f3759a952029c4944de3e68c12c2
echovenancio <venancio@local>
2025-03-19 11:51:20 -0300

    firestore rest funcs

--- server/database.py
+++ server/database.py
@@ -28,7 +28,6 @@ def get_headers(auth_token):
         "Content-Type": "application/json"
     }
 
-<<<<<<< HEAD
 class GenericDBManager(ABC):
     @abstractmethod
     def login(self, email, password) -> LoginRes:
