6d8e1f8077256b24af96945c529b1a8f586fb80b
pcfatec <pcfatec@e.com>
2025-05-15 15:42:13 -0300

    rename repository to service

--- server/database/manager_getter.py
+++ server/database/manager_getter.py
@@ -3,8 +3,8 @@ from config import settings
 from database.generic_repository import GenericDBManager
 
 def get_db_manager() -> GenericDBManager:
-    from database.sqlite_repository import DevDBManager
-    from database.firestore_repository import FirestoreDBManager
+    from database.sqlite_service import DevDBManager
+    from database.firestore_service import FirestoreDBManager
 
     if settings.env == 'dev' or settings.env == 'test':
         return DevDBManager()
